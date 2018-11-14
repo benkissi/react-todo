830007dae4652ddfa10d81086ec6a36a2c242bc1
benkissi <benkissi111@gmail.com>
2018-11-14 03:28:29 +0000

    Refactored TodoList, Todo and AddTodo to work with redux

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
 var moment = require('moment');
+var {connect} = require('react-redux');
+var actions = require('actions');
 
 
-var Todo = React.createClass({
+export var Todo = React.createClass({
     render: function() {
-        var {text, id, completed, createdAt, completedAt} = this.props;
+        var {text, id, completed, createdAt, completedAt, dispatch} = this.props;
         var todoClassName = completed ? "todo todo-completed" : "todo";
         var renderDate = () => {
             var message= 'created ';
@@ -18,7 +20,10 @@ var Todo = React.createClass({
             return message + moment.unix(timeStamp).format('MMM Do YYYY @ h:mm a');
         }
         return(
-            <div className={todoClassName} onClick={()=> {this.props.onToggle(id);}}>
+            <div className={todoClassName} onClick={()=> {
+                    // this.props.onToggle(id);
+                    dispatch(actions.toggleTodo(id));
+                }}>
                 <div>
                     <input type="checkbox" checked={completed}/>
                 </div>
@@ -31,4 +36,5 @@ var Todo = React.createClass({
     }
 });
 
-module.exports = Todo;
+export default connect()(Todo);
+
