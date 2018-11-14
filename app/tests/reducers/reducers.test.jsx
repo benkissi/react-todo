bd45378b213c9e9768624db29af7f0afe4b9ebeb
benkissi <benkissi111@gmail.com>
2018-11-14 05:12:16 +0000

    refactor app to work with local storage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -50,7 +50,7 @@ describe('Reducers', () => {
                 createdAt: 123,
                 completedAt: 125
 
-            }]
+            }];
             var action = {
                 type: 'TOGGLE_TODO',
                 id: '123'
@@ -60,8 +60,26 @@ describe('Reducers', () => {
             
             expect(res[0].completed).toEqual(false);
             expect(res[0].completedAt).toEqual(undefined);
-
-         
         })
+
+        it('should add existing todos', ()=> {
+            var todos = [{
+                id: '111',
+                text: 'anything',
+                completed: false,
+                completedAt: undefined,
+                createdAt: 33000
+            }];
+    
+            var action = {
+                type: 'ADD_TODOS',
+                todos
+            };
+
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0]).toEqual(todos[0]);
+        });
     })
 });
